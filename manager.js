71b49168d2b998bfce5de2f3af000b65c55e8f52
sujal_shrestha <sujal.scxi121@amv.edu.np>
2023-02-23 15:45:54 +0545

    Completed

---
+++ manager.js
@@ -0,0 +1,23 @@
+const Employee=require('./employee.js');
+class Manager extends Employee{
+    constructor(name,salary,title,manager){
+        super(name,salary,title,manager);
+        this.employees=[];
+
+    }
+    addEmployee(employee){
+        this.employees.push(employee);
+    }
+}
+
+const splinter = new Manager('Splinter', 100000, 'Sensei');
+console.log('Before: ', splinter);
+
+const leo = new Employee('Leonardo', 90000, 'Ninja', splinter);
+const mikey = new Employee('Michelangelo', 90000, 'Ninja', splinter);
+const donnie = new Employee('Donatello', 90000, 'Ninja', splinter);
+const raph = new Employee('Raphael', 90000, 'Ninja', splinter);
+
+console.log('After: ', splinter);
+
+module.exports=Manager;
